727467581e7d302c925e7be77e9dfb24fc3a7bdd
Harshit Garg <harshitgargmnit@gmail.com>
2017-02-06 10:21:15 +0530

    complete pagination

--- games/views.py
+++ games/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from django.views import generic
+from .models import Game 
+from django.core.paginator import Paginator
+
 
 def home(request):
-	return render(request, "base.html")
+	return render(request, "games/home.html")
 	return HttpResponse("Hello")
+
+
+class GamesListView(generic.ListView):
+
+    template_name = 'games/games_list.html'
+    context_object_name = 'games_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        """Return all the objects.""" 
+        return Game.objects.all()
